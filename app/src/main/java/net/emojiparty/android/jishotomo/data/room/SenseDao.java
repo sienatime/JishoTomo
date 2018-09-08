51e65d18022b9cf24254a4690b6cd13ae8239962
Siena Aguayo <siena.aguayo@gmail.com>
2018-09-07 22:08:45 -0700

    k that is why xrefs were broken

--- app/src/main/java/net/emojiparty/android/jishotomo/data/room/SenseDao.java
+++ app/src/main/java/net/emojiparty/android/jishotomo/data/room/SenseDao.java
@@ -8,7 +8,7 @@ import net.emojiparty.android.jishotomo.data.models.CrossReferencedEntry;
 
 @Dao public interface SenseDao {
   @Query("SELECT mainSenses.id AS senseId,"
-      + "  xRefEntries.id AS id, xRefEntries.primary_kanji AS primaryKanji, xRefEntries.primary_reading AS primaryReading "
+      + "  xRefEntries.id AS id, xRefEntries.primary_kanji, xRefEntries.primary_reading "
       + "  FROM senses AS mainSenses"
       + "  JOIN cross_references ON cross_references.sense_id = mainSenses.id"
       + "  JOIN senses AS xRefSenses ON xRefSenses.id = cross_references.cross_reference_sense_id"
