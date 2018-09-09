e4c629ec84f0a3018cf19b6dd085d3ca6aa6c735
Siena Aguayo <siena.aguayo@gmail.com>
2018-09-08 17:28:16 -0700

    start to customize drawer

--- app/src/main/java/net/emojiparty/android/jishotomo/ui/activities/DrawerActivity.java
+++ app/src/main/java/net/emojiparty/android/jishotomo/ui/activities/DrawerActivity.java
@@ -114,17 +114,11 @@ public class DrawerActivity extends AppCompatActivity
     // Handle navigation view item clicks here.
     int id = item.getItemId();
 
-    if (id == R.id.nav_camera) {
+    if (id == R.id.nav_search) {
       // Handle the camera action
-    } else if (id == R.id.nav_gallery) {
+    } else if (id == R.id.nav_browse) {
 
-    } else if (id == R.id.nav_slideshow) {
-
-    } else if (id == R.id.nav_manage) {
-
-    } else if (id == R.id.nav_share) {
-
-    } else if (id == R.id.nav_send) {
+    } else if (id == R.id.nav_favorites) {
 
     }
 
