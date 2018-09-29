86b5e81b5a8ece14d03670655ea889e19469277e
Siena Aguayo <siena.aguayo@gmail.com>
2018-09-29 13:30:57 -0700

    not using this inject atm

--- app/src/main/java/net/emojiparty/android/jishotomo/data/di/AppComponent.java
+++ app/src/main/java/net/emojiparty/android/jishotomo/data/di/AppComponent.java
@@ -7,13 +7,11 @@ import net.emojiparty.android.jishotomo.data.AppRepository;
 import net.emojiparty.android.jishotomo.data.room.AppDatabase;
 import net.emojiparty.android.jishotomo.data.room.EntryDao;
 import net.emojiparty.android.jishotomo.data.room.SenseDao;
-import net.emojiparty.android.jishotomo.ui.activities.DrawerActivity;
 
 // https://medium.com/@marco_cattaneo/integrate-dagger-2-with-room-persistence-library-in-few-lines-abf48328eaeb
 @Singleton
 @Component(dependencies = {}, modules = {AppModule.class, RoomModule.class})
 public interface AppComponent {
-  void inject(DrawerActivity drawerActivity);
   void inject(AppRepository appRepository);
   EntryDao entryDao();
   SenseDao senseDao();
