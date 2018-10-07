ab89311279d8ac0dd673c9ee4746a776a2ca5c26
Siena Aguayo <siena.aguayo@gmail.com>
2018-10-07 14:35:01 -0700

    refactor cross-reference links to be buttons so that they are more accessible

--- app/src/main/java/net/emojiparty/android/jishotomo/ui/presentation/BindingMethods.java
+++ app/src/main/java/net/emojiparty/android/jishotomo/ui/presentation/BindingMethods.java
@@ -1,19 +1,27 @@
 package net.emojiparty.android.jishotomo.ui.presentation;
 
 import android.content.Context;
+import android.content.Intent;
 import android.databinding.BindingAdapter;
 import android.text.Html;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
 import android.text.style.LocaleSpan;
+import android.view.ContextThemeWrapper;
 import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import java.util.List;
 import java.util.Locale;
 import net.emojiparty.android.jishotomo.R;
 import net.emojiparty.android.jishotomo.data.SemicolonSplit;
 import net.emojiparty.android.jishotomo.data.models.CrossReferencedEntry;
+import net.emojiparty.android.jishotomo.ui.activities.DefinitionActivity;
+
+import static net.emojiparty.android.jishotomo.ui.activities.DefinitionActivity.ENTRY_ID_EXTRA;
 
 public class BindingMethods {
   @BindingAdapter({ "visibleOrGone" })
@@ -24,31 +32,38 @@ public class BindingMethods {
 
   @BindingAdapter({ "crossReferenceLinks" }) public static void setCrossReferenceLinks(View view,
       List<CrossReferencedEntry> crossReferencedEntries) {
-    if (crossReferencedEntries == null || crossReferencedEntries.size() == 0) {
-      view.setVisibility(View.GONE);
-    } else {
-      view.setVisibility(View.VISIBLE);
+    LinearLayout linearLayout = (LinearLayout) view;
+    if (crossReferencedEntries == null || crossReferencedEntries.size() == 0 || linearLayout.getChildCount() > 0) {
+      return;
+    }
 
-      Context context = view.getContext();
-      String seeAlso = context.getString(R.string.see_also);
-      String links = CrossReferenceText.format(crossReferencedEntries);
+    Context context = view.getContext();
+    int buttonStyle = R.style.xref_button;
 
-      String allText = String.format(context.getString(R.string.see_also_format), seeAlso, links);
-      Spanned spanned = Html.fromHtml(allText);
-      SpannableString mixedLocaleSpan = MixedLocaleSpan.format(spanned, seeAlso);
-
-      TextView textView = (TextView) view;
-      textView.setText(mixedLocaleSpan);
-      textView.setMovementMethod(LinkMovementMethod.getInstance());
+    for (int i = 0; i < crossReferencedEntries.size(); i++) {
+      CrossReferencedEntry crossReferencedEntry = crossReferencedEntries.get(i);
+      Button button = new Button(new ContextThemeWrapper(context, buttonStyle), null, buttonStyle);
+      SpannableString spannable = new SpannableString(crossReferencedEntry.getKanjiOrReading());
+      spannable.setSpan(new LocaleSpan(Locale.JAPANESE), 0, spannable.length(), 0);
+      button.setText(spannable);
+      button.setOnClickListener((View clickView) -> {
+        Intent intent = new Intent(context, DefinitionActivity.class);
+        intent.putExtra(ENTRY_ID_EXTRA, crossReferencedEntry.id);
+        context.startActivity(intent);
+      });
+      linearLayout.addView(button);
     }
   }
 
-  @BindingAdapter({ "appliesToText" }) public static void setAppliesToText(View view, String appliesTo) {
+  @BindingAdapter({ "appliesToText" })
+  public static void setAppliesToText(View view, String appliesTo) {
     TextView textView = (TextView) view;
     Context context = view.getContext();
     String literalAppliesTo = context.getString(R.string.applies_to);
     String allAppliesTo = SemicolonSplit.splitAndJoin(appliesTo);
-    String formatted = String.format(context.getString(R.string.applies_to_format), literalAppliesTo, allAppliesTo);
+    String formatted =
+        String.format(context.getString(R.string.applies_to_format), literalAppliesTo,
+            allAppliesTo);
 
     SpannableString spannableString = MixedLocaleSpan.format(formatted, literalAppliesTo);
     textView.setText(spannableString);
@@ -65,7 +80,8 @@ public class BindingMethods {
   }
 
   // so that TalkBack will read these TextViews in Japanese
-  @BindingAdapter({ "textInJapanese" }) public static void setTextWithJapanese(View view, String text) {
+  @BindingAdapter({ "textInJapanese" }) public static void setTextWithJapanese(View view,
+      String text) {
     if (text != null) {
       TextView textView = (TextView) view;
       SpannableString spannable = new SpannableString(text);
