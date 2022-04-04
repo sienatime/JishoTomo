d609798dfba4172457f813880fdf8b0646314a1a
Siena Aguayo <siena.aguayo@gmail.com>
2022-04-04 11:34:59 -0700

    update more android deps

--- app/src/main/java/net/emojiparty/android/jishotomo/ui/viewmodels/EntryViewModelFactory.kt
+++ app/src/main/java/net/emojiparty/android/jishotomo/ui/viewmodels/EntryViewModelFactory.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 
 class EntryViewModelFactory(private val entryId: Int) : ViewModelProvider.Factory {
   @Suppress("UNCHECKED_CAST")
-  override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+  override fun <T : ViewModel> create(modelClass: Class<T>): T {
     return EntryViewModel(entryId) as T
   }
 }
--- app/src/main/java/net/emojiparty/android/jishotomo/ui/viewmodels/PagedEntriesViewModelFactory.kt
+++ app/src/main/java/net/emojiparty/android/jishotomo/ui/viewmodels/PagedEntriesViewModelFactory.kt
@@ -8,7 +8,7 @@ class PagedEntriesViewModelFactory(
   private val resourceFetcher: ResourceFetcher
 ) : ViewModelProvider.Factory {
   @Suppress("UNCHECKED_CAST")
-  override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+  override fun <T : ViewModel> create(modelClass: Class<T>): T {
     return PagedEntriesViewModel(resourceFetcher) as T
   }
 }
